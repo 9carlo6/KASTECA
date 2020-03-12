200d73e46026a23a43b082fda2033474155d711b
9carlo6 <carlopannullo809@gmail.com>
2020-03-12 15:57:25 +0100

    T1 prova per icescrum

--- app/src/main/java/com/kasteca/MainActivity.java
+++ app/src/main/java/com/kasteca/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         prefs= getPreferences(MODE_PRIVATE);
         email_edit_text = findViewById(R.id.Email_Edit_Text);
         password_edit_text = findViewById(R.id.Password_Edit_Text);
-        password_edit_text.setText("password");// lo teniamo per velocizzare, ma andra cancellato
+        password_edit_text.setText("password"); // lo teniamo per velocizzare, ma andra cancellato
         email_edit_text.setText(prefs.getString(LAST_USER,""));
 
         // testo che rimanda all'activity per la creazione di un nuovo account (RegistrationActivity)
