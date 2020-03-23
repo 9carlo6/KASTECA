21f02db4f31f599c6865e8d5966f02dac0f7f3df
9carlo6 <carlopannullo809@gmail.com>
2020-03-23 15:17:44 +0100

    T5 creazione activity login studente con caricamento dei dati necessari per gestire lo studente autenticato

--- app/src/main/java/com/kasteca/LogStudenteActivity.java
+++ app/src/main/java/com/kasteca/LogStudenteActivity.java
@@ -12,7 +12,6 @@ package com.kasteca;
         import android.view.MenuItem;
         import android.view.View;
         import android.widget.TextView;
-        import android.widget.Toast;
 
         import com.google.android.material.navigation.NavigationView;
 
